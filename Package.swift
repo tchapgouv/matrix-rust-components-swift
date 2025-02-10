07925c708a499ca2fe2ac2707b6800f0cf209022
Nicolas Buquet <nbuquet@buquet-net.com>
2025-02-10 15:06:21 +0100

    Bump to version 20250127c (matrix-rust-sdk/add-room-access-rules-ffi 2fa993cf1287e9e571049403c4fb75cd96b0670f)

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+let checksum = "29e242eadc04cd32d2d168b6cd51f3a4e0517663f10a386892ac50b9e735289d"
+let version = "20250127c"
+let localpath = "MatrixSDKFFI.xcframework.zip"
+let package = Package(
+    name: "MatrixRustSDK",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(name: "MatrixRustSDK", type: .dynamic, targets: ["MatrixRustSDK"]),
+    ],
+    targets: [
+        .binaryTarget(name: "MatrixSDKFFI", path: localpath),
+        .target(name: "MatrixRustSDK", dependencies: [.target(name: "MatrixSDKFFI")])
+    ]
+)
