c17225e0f09c1cca613ffa0df66a5f84fd50e3f7
Nicolas Buquet <nbuquet@buquet-net.com>
2025-03-25 14:56:04 +0100

    Bump to version 25.3.25-contentscanner-02 (matrix-rust-sdk/nicos/ios-test-content-scanner 7bb5e801fdd64ca39c72f32f799c5ab212200191)

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "a44df787afd4581bc927b09ce6e7dbfc87a0338b97cf9e24c39bc072c585b6ed"
+let checksum = "4ff6e2070de95a8204776dd924f940c8508554ab5fc518d3799b7b9fd38202a5"
 let version = "25.3.25-contentscanner-02"
 let url = "https://github.com/tchapgouv/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
