fddd659b40171f908874944ac6bdf2396281e141
Nicolas Buquet <nbuquet@buquet-net.com>
2025-05-15 18:39:10 +0200

    Bump to version 20250515a (matrix-rust-sdk/nicos/bwi-0-8-1-matrix-20250408 770aebcc28d70ee76631ddf1b731b8d5b1db34f4)

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "dfefccf2032c4fbbdaf31fb8a8b2601ef9b42a39206ce4b8f0eb6d2d29c297c6"
-let version = "25.04.11-bwi-0-8-1"
+let checksum = "8f3211d2d101c9e9202e5fcde3c776f139ca68d414ee96c3cc54a4d9fc2fc604"
+let version = "20250515a"
 let url = "https://github.com/tchapgouv/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
