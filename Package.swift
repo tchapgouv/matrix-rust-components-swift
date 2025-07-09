74f35fa913d0a12aa9e4523675a3e45df231dee9
Nicolas Buquet <github@buquet-net.com>
2025-07-09 16:22:02 +0200

    Bump to version 25.04.30-bwi-0-8-1 (matrix-rust-sdk/nicos/bwi-0-8-1 8e33c93fefdd505e4bc82e1570704fbc773606d4)

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "ea39df34e4163f24eed172f1ac3048f3a20274502c447336e257917b0b824fb1"
-let version = "25.04.30"
+let checksum = "9d900b8cb631c7acc4367b654c2f8423ea423512bc0057263e20d65eedec5033"
+let version = "25.04.30-bwi-0-8-1"
 let url = "https://github.com/tchapgouv/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
@@ -13,7 +13,8 @@ let package = Package(
         .library(name: "MatrixRustSDK", type: .dynamic, targets: ["MatrixRustSDK"]),
     ],
     targets: [
-        .binaryTarget(name: "MatrixSDKFFI", url: url, checksum: checksum),
+//        .binaryTarget(name: "MatrixSDKFFI", url: url, checksum: checksum),
+        .binaryTarget(name: "MatrixSDKFFI", path: "MatrixSDKFFI.xcframework.zip"),
         .target(name: "MatrixRustSDK", dependencies: [.target(name: "MatrixSDKFFI")])
     ]
 )
