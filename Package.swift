7d3bd30f4851f16da990eced55b1e320cfad5111
Nicolas Buquet <github@buquet-net.com>
2025-07-09 16:32:46 +0200

    Bump to version 25.04.30-bwi-0-8-1 (matrix-rust-sdk/nicos/bwi-0-8-1 6f21cbc33354f783e10121b6aed7ad421ecd7508)

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "a29b595438017ac223a3a37429ba0fddc8a3c6bdc8acdae049e74095761a8152"
+let checksum = "a89531e4848bcace8dd7e2ec900f5c150b62c471f878acbbfc622c27e5ae3a5b"
 let version = "25.04.30-bwi-0-8-1"
 let url = "https://github.com/tchapgouv/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
